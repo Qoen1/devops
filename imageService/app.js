f0570280c002b30ab282d8b9217826fa7609b225
qoen <k.verstappen1@student.avans.nl>
2024-04-08 17:23:54 +0200

    alle initialisatie gemaakt om te kunnen gaan ontwikkelen

--- imageService/app.js
+++ imageService/app.js
@@ -1,30 +1,32 @@
-﻿const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const PORT = process.env.PORT || 3001;
-const DB_URL = process.env.DB_URL;
+﻿const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const PORT = process.env.PORT
+const DB_URL = process.env.DB_URL
 
 mongoose.connect(DB_URL);
 
-const app = express();
+const app = express()
 
 
 var jsonParser = bodyParser.json()
 app.use(jsonParser)
 
 //routes
-
+app.get('/yeet', (request, response) => {
+    response.send('YEETT')
+})
 
 //end routes
 
 //error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`)
+})
---
+++ messageService/app.js
@@ -0,0 +1,32 @@
+﻿const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const PORT = process.env.PORT
+const DB_URL = process.env.DB_URL
+
+mongoose.connect(DB_URL)
+
+const app = express()
+
+
+var jsonParser = bodyParser.json()
+app.use(jsonParser)
+
+//routes
+app.get('/yeet', (request, response) => {
+    response.send('YEETT')
+})
+
+//end routes
+
+//error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
