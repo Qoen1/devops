4d243226035c5fe6ff9f0ff7b72e32102d081f75
qoen <k.verstappen1@student.avans.nl>
2024-04-09 15:50:19 +0200

    reee

--- gateway/app.js
+++ gateway/app.js
@@ -1,15 +1,25 @@
 ﻿const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config()
+const promBundle = require('express-prom-bundle');
+
 const PORT = process.env.PORT
 
 const app = express()
-
-
 var jsonParser = bodyParser.json()
+const metrics_middleware = promBundle({
+    includePath: true,
+    includeStatusCode: true,
+    normalizePath: true,
+    promClient: {
+        collectDefaultMetrics: {}
+    }
+})
+
 app.use(jsonParser)
 
 //routes
+app.use(metrics_middleware)
 app.get('/yeet', (request, response) => {
     response.send('YEETT')
 })
--- imageService/app.js
+++ imageService/app.js
@@ -2,18 +2,28 @@
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('dotenv').config()
+const promBundle = require('express-prom-bundle');
+
 const PORT = process.env.PORT
 const DB_URL = process.env.DB_URL
 
 mongoose.connect(DB_URL).then(x => console.log(`connected to database ${DB_URL}`))
 
 const app = express()
+const jsonParser = bodyParser.json()
+const metrics_middleware = promBundle({
+    includePath: true,
+    includeStatusCode: true,
+    normalizePath: true,
+    promClient: {
+        collectDefaultMetrics: {}
+    }
+})
 
-
-var jsonParser = bodyParser.json()
 app.use(jsonParser)
 
 //routes
+app.use(metrics_middleware)
 app.get('/yeet', (request, response) => {
     response.send('YEETT')
 })
